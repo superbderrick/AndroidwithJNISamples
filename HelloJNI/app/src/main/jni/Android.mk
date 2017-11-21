586679f1d66182ff79dcd430a73912242713099e
derrick <kang.derrick@gmail.com>
2017-11-21 19:30:11 +0900

    added test coded

--- HelloJNI/app/src/main/jni/Android.mk
+++ HelloJNI/app/src/main/jni/Android.mk
@@ -5,5 +5,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := NDKTest
 LOCAL_SRC_FILES := main.cpp
 LOCAL_LDLIBS := -llog
-NDK_TOOLCHAIN_VERSION := clang
+
 include $(BUILD_SHARED_LIBRARY)
--- HelloJNI/app/src/main/jni/Application.mk
+++ HelloJNI/app/src/main/jni/Application.mk
@@ -1,3 +1,4 @@
-APP_ABI=all
-
+APP_ABI= arm64-v8a armeabi-v7a x86 
+APP_STL := gnustl_shared
+NDK_TOOLCHAIN_VERSION := clang
 APP_PLATFORM = android-14
